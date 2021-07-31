b19ddcd34da3b7a730508393eeb98d349e4fa67e
E1-Bart0 <starovoitov.vadik1@gmail.com>
2021-07-31 21:21:35 +0300

    migration and settings

--- alias/conf/settings.py
+++ alias/conf/settings.py
@@ -122,4 +122,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "frontend/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
